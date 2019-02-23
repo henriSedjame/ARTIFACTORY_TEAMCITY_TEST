25a3f5b640688cf85311c4d2fa660b68dda4ed37
henriSedjame <sedhjo@gmail.com>
2019-02-23 19:40:52 +0100

    Initial commit

---
+++ src/main/java/io/github/hsedjame/artifactory/Personne.java
@@ -0,0 +1,46 @@
+package io.github.hsedjame.artifactory;
+
+/**
+ * @Project ARTIFACTORY_TEST
+ * @Author Henri Joel SEDJAME
+ * @Date 23/02/2019
+ * @Class purposes : .......
+ */
+public class Personne {
+    private String nom;
+    private String prenom;
+    private int age;
+
+    public Personne(String nom, String prenom, int age) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+    }
+
+    public Personne() {
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ src/main/java/io/github/hsedjame/artifactory/PersonneService.java
@@ -0,0 +1,16 @@
+package io.github.hsedjame.artifactory;
+
+import java.util.function.Consumer;
+
+/**
+ * @Project ARTIFACTORY_TEST
+ * @Author Henri Joel SEDJAME
+ * @Date 23/02/2019
+ * @Class purposes : .......
+ */
+public class PersonneService {
+
+    public void presenter(Personne personne, Consumer<Personne> presentationconsumer){
+        presentationconsumer.accept(personne);
+    }
+}
