13db4de21db5a1e8e98b5bf12a05ba53f23fcdc2
henriSedjame <sedhjo@gmail.com>
2019-02-28 19:24:59 +0100

    commenatire ajouté

--- src/main/java/io/github/hsedjame/artifactory/PersonneService.java
+++ src/main/java/io/github/hsedjame/artifactory/PersonneService.java
@@ -11,7 +11,7 @@ import java.util.function.Consumer;
 public class PersonneService {
 
     public void presenter(Personne personne, Consumer<Personne> presentationconsumer){
-        //Comentaire juste pour commit
+
         presentationconsumer.accept(personne);
     }
 
